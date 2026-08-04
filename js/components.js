c651b1ec4469437fefc7099d2d95bb0e9c2451ee
Awais Ahmad <callawaisahmad@gmail.com>
2026-08-04 18:13:43 +0500

    feat: link Facebook page (facebook.com/abroadreadyorg) in footer + contact (build #22)

--- js/components.js
+++ js/components.js
@@ -142,7 +142,7 @@
         '<a href="' + HOME + '" class="site-logo"><span class="site-logo-icon">' + LOGO_MARK + '</span><span class="site-logo-text">Abroad<span class="text-gradient">Ready</span></span></a>' +
           '<p class="site-footer-tag">A free scholarship board — find funding, deadlines, eligibility and how to apply, all in one place.</p>' +
           '<div class="site-social">' +
-            socialIcon("Facebook", "M24 12.07C24 5.4 18.63 0 12 0S0 5.4 0 12.07c0 6 4.39 10.97 10.13 11.85v-8.38H7.08v-3.47h3.05V9.43c0-3.01 1.79-4.67 4.53-4.67 1.31 0 2.69.24 2.69.24v2.95h-1.52c-1.49 0-1.96.93-1.96 1.87v2.25h3.33l-.53 3.47h-2.8V24C19.61 23.04 24 18.07 24 12.07z") +
+            socialIcon("Facebook", "M24 12.07C24 5.4 18.63 0 12 0S0 5.4 0 12.07c0 6 4.39 10.97 10.13 11.85v-8.38H7.08v-3.47h3.05V9.43c0-3.01 1.79-4.67 4.53-4.67 1.31 0 2.69.24 2.69.24v2.95h-1.52c-1.49 0-1.96.93-1.96 1.87v2.25h3.33l-.53 3.47h-2.8V24C19.61 23.04 24 18.07 24 12.07z", "https://www.facebook.com/abroadreadyorg") +
             socialIcon("Twitter", "M18.24 2.25h3.3l-7.22 8.26L23 21.75h-6.63l-5.2-6.82-5.95 6.82H1.9l7.73-8.84L1 2.25h6.79l4.7 6.23zM17.1 19.77h1.83L7.03 4.13H5.06z") +
             socialIcon("LinkedIn", "M20.45 20.45h-3.55v-5.57c0-1.33-.03-3.04-1.85-3.04-1.86 0-2.14 1.44-2.14 2.94v5.67H9.36V9h3.41v1.56h.05c.48-.9 1.64-1.85 3.37-1.85 3.6 0 4.27 2.37 4.27 5.46zM5.34 7.43a2.06 2.06 0 11 0-4.13 2.06 2.06 0 010 4.13zM7.12 20.45H3.56V9h3.56zM22.22 0H1.77C.79 0 0 .77 0 1.73v20.54C0 23.23.79 24 1.77 24h20.45c.98 0 1.78-.77 1.78-1.73V1.73C24 .77 23.2 0 22.22 0z") +
             socialIcon("Instagram", "M12 2.16c3.2 0 3.58.01 4.85.07 3.25.15 4.77 1.69 4.92 4.92.06 1.27.07 1.65.07 4.85s-.01 3.58-.07 4.85c-.15 3.23-1.66 4.77-4.92 4.92-1.27.06-1.64.07-4.85.07s-3.58-.01-4.85-.07c-3.26-.15-4.77-1.7-4.92-4.92-.06-1.27-.07-1.64-.07-4.85s.01-3.58.07-4.85C2.4 3.94 3.9 2.38 7.15 2.23 8.42 2.17 8.8 2.16 12 2.16zM12 0C8.74 0 8.33.01 7.05.07 2.7.27.27 2.69.07 7.05.01 8.33 0 8.74 0 12s.01 3.67.07 4.95c.2 4.36 2.62 6.78 6.98 6.98C8.33 23.99 8.74 24 12 24s3.67-.01 4.95-.07c4.35-.2 6.78-2.62 6.98-6.98.06-1.28.07-1.69.07-4.95s-.01-3.67-.07-4.95c-.2-4.35-2.62-6.78-6.98-6.98C15.67.01 15.26 0 12 0zm0 5.84a6.16 6.16 0 100 12.32 6.16 6.16 0 000-12.32zM12 16a4 4 0 110-8 4 4 0 010 8zm6.41-11.85a1.44 1.44 0 100 2.88 1.44 1.44 0 000-2.88z") +
@@ -184,8 +184,10 @@
     return '<div class="site-footer-col"><h5>' + title + "</h5>" +
       links.map(function (l) { return '<a href="' + l[1] + '">' + l[0] + "</a>"; }).join("") + "</div>";
   }
-  function socialIcon(label, path) {
-    return '<span class="site-social-link" aria-label="' + label + '" title="' + label + '"><svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="' + path + '"/></svg></span>';
+  function socialIcon(label, path, url) {
+    var icon = '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="' + path + '"/></svg>';
+    if (!url) return '<span class="site-social-link" aria-label="' + label + '" title="' + label + '">' + icon + '</span>';
+    return '<a class="site-social-link" href="' + url + '" target="_blank" rel="noopener" aria-label="' + label + '" title="' + label + '">' + icon + '</a>';
   }
 
   // ---------- MOUNT ----------
